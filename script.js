a81c15b47f359c3abf3f1562d4def5d741c6a882
Bruno Henrique <92992575+brunohenri08@users.noreply.github.com>
2025-07-04 23:08:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+//classList.toggle('hidden') adiciona a classe hidden se ela não estiver presente, ou remove se já estiver.
+function alteraAparenciaMenu(id) {
+        document.getElementById(id).classList.toggle('hidden');
+}
